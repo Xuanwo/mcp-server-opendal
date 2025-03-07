ab2736029a4d63a1b01834a2b90d2dbe782b8e22
Frost Ming <me@frostming.com>
2025-03-07 09:02:24 +0800

    chore: add example dependencies and update the instructions
    Signed-off-by: Frost Ming <me@frostming.com>

--- examples/llamaindex-with-opendal-mcp.py
+++ examples/llamaindex-with-opendal-mcp.py
@@ -1,41 +1,26 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "python-dotenv",
+#     "llama-index",
+#     "llama-index",
+#     "llama-index-llms-openai-like",
+#     "llama-index-tools-mcp"
+# ]
+# ///
+
 import asyncio
 import os
 
-from llama_index.tools.mcp import McpToolSpec
-from llama_index.core.llms import ChatMessage
-from llama_index.llms.openai_like import OpenAILike
+from dotenv import load_dotenv
 from llama_index.core.agent import ReActAgent, ReActChatFormatter
 from llama_index.core.agent.react.prompts import REACT_CHAT_SYSTEM_HEADER
-from llama_index.tools.mcp import McpToolSpec, BasicMCPClient
-from dotenv import load_dotenv
+from llama_index.core.llms import ChatMessage
+from llama_index.llms.openai_like import OpenAILike
+from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
 
 load_dotenv()
 
-# To run this example, you need to have a MCP server running.
-# You can start a MCP server by running `mcp-server-opendal` in the root directory.
-#
-# And set the environment variables first.
-# - OPENDAL_FS_TYPE=fs
-# - OPENDAL_FS_ROOT=./examples/
-#
-# Then, run the following command:
-#
-# ```bash
-# python ./src/mcp_server_opendal/server.py --transport sse
-# ```
-#
-# And set the environment variables below.
-# - MCP_HOST: The host of the MCP server
-# - MCP_PORT: The port of the MCP server
-# - OPENAI_API_KEY: The API key of the OpenAI API
-# - OPENAI_MODEL: The model of the OpenAI API
-# - OPENAI_ENDPOINT: The endpoint of the OpenAI API
-#
-# Then, run the following command:
-#
-# ```bash
-# python examples/llamaindex-with-opendal-mcp.py
-# ```
 
 # MCP Server Connection Parameters
 MCP_HOST = os.getenv("MCP_HOST")
@@ -54,7 +39,12 @@ then read the necessary CSV files, and summarize the main findings.
 async def get_agent(tools: McpToolSpec):
     tools = await tools.to_tool_list_async()
     agent = ReActAgent.from_tools(
-        llm=OpenAILike(model=os.getenv("OPENAI_MODEL"), api_base=os.getenv("OPENAI_ENDPOINT"), api_key=os.getenv("OPENAI_API_KEY"), is_chat_model=True),
+        llm=OpenAILike(
+            model=os.getenv("OPENAI_MODEL"),
+            api_base=os.getenv("OPENAI_ENDPOINT"),
+            api_key=os.getenv("OPENAI_API_KEY"),
+            is_chat_model=True,
+        ),
         tools=list(tools),
         react_chat_formatter=ReActChatFormatter(
             system_header=SYSTEM_PROMPT + "\n" + REACT_CHAT_SYSTEM_HEADER,
@@ -69,6 +59,7 @@ async def handle_user_message(message_content: str, agent: ReActAgent):
     user_message = ChatMessage.from_str(role="user", content=message_content)
     response = await agent.achat(message=user_message.content)
     print(response.response)
+
 
 async def main():
     mcp_tool = McpToolSpec(client=BasicMCPClient(f"http://{MCP_HOST}:{MCP_PORT}/sse"))
